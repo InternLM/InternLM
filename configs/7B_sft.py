a495341c3fcc9d260327790de223132296591ba9
wangguoteng.p <wangguoteng925@qq.com>
2023-07-08 03:59:18 +0800

    feat(config): add checkpoint_fraction into config

--- configs/7B_sft.py
+++ configs/7B_sft.py
@@ -97,7 +97,8 @@ beta2_scheduler = dict(
 )
 
 model = dict(
-    checkpoint=False,
+    checkpoint=False,  # The proportion of layers for activation aheckpointing, the optional value are True/False/[0-1]
+    checkpoint_fraction=0,
     num_attention_heads=NUM_ATTENTION_HEAD,
     embed_split_hidden=True,
     vocab_size=VOCAB_SIZE,
