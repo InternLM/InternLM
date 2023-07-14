7bac1ceff3bbd4efdeb9e04c712c3dc1e8b0b004
LYMDLUT <70597027+LYMDLUT@users.noreply.github.com>
2023-07-14 17:37:34 +0800

    Update web_demo.py

--- web_demo.py
+++ web_demo.py
@@ -151,9 +151,9 @@ class GenerationConfig:
     
 
 @st.cache_resource
-def load_model(model):
-    model = AutoModelForCausalLM.from_pretrained(model, trust_remote_code=True).to(torch.bfloat16).cuda()
-    tokenizer = AutoTokenizer.from_pretrained(model, trust_remote_code=True)
+def load_model(model_path):
+    model = AutoModelForCausalLM.from_pretrained(model_path, trust_remote_code=True).to(torch.bfloat16).cuda()
+    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
     return model, tokenizer
 
 
