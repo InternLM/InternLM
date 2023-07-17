0c1060435d17a620a0eed5b07ab581055ca30c08
x54-729 <45304952+x54-729@users.noreply.github.com>
2023-07-17 21:08:10 +0800

    Use tempfile for convert2hf.py (#23)
    Fix https://github.com/InternLM/InternLM/issues/50

--- tools/transformers/modeling_internlm.py
+++ tools/transformers/modeling_internlm.py
@@ -31,7 +31,7 @@ from transformers.modeling_outputs import BaseModelOutputWithPast, CausalLMOutpu
 from transformers.modeling_utils import PreTrainedModel
 from transformers.generation.streamers import BaseStreamer
 from transformers.utils import add_start_docstrings, add_start_docstrings_to_model_forward, logging, replace_return_docstrings
-from .configuration_internlm import InternLMConfig
+from configuration_internlm import InternLMConfig
 
 
 logger = logging.get_logger(__name__)
