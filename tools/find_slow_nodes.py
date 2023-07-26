84b33bbd16d2e256fbd76bc693a9b2fa4bab2678
JiaoPL <jplzyc@163.com>
2023-07-26 09:35:27 +0000

    support aliyun dlc nccl test

--- tools/find_slow_nodes.py
+++ tools/find_slow_nodes.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 import argparse
 import copy
 import fcntl
@@ -7,8 +8,15 @@ from torch import distributed as dist
 import os
 import socket
 import re
+import base64
+import hashlib
+import hmac
+import uuid
+import requests
 
+from datetime import datetime
 from typing import List
+from configparser import ConfigParser
 
 def printflock(*msgs,flush: bool=False):
     """ print """
@@ -18,6 +26,129 @@ def printflock(*msgs,flush: bool=False):
             print(*msgs, flush=flush)
         finally:
             fcntl.flock(fh, fcntl.LOCK_UN)
+
+class DLCJobInfo:
+    """
+    get dlc job detailed information
+    """
+
+    def __init__(
+        self,
+        endpoint,
+        access_key_id,
+        access_key_secret,
+        protocol,
+    ):
+        self.endpoint = endpoint
+        self.access_key_id = access_key_id
+        self.access_key_secret = access_key_secret
+        self.protocol = protocol
+
+    def _get_canonicalized_resource(self, pathname, query):
+        if len(query) <= 0:
+            return pathname
+        resource = f"{pathname}?"
+        query_list = sorted(list(query))
+        for key in query_list:
+            if query[key] is not None:
+                if query[key] == "":
+                    s = f"{key}&"
+                else:
+                    value = self.to_string(query[key])
+                    s = f"{key}={value}&"
+            resource += s
+        return resource[:-1]
+
+    def to_string(self, s, encoding="utf-8"):
+        if s is None:
+            return s
+        if isinstance(s, bytes):
+            return s.decode(encoding)
+        else:
+            return str(s)
+
+    def get_string_to_sign(self, method, url_path, headers, query):
+        """使用请求信息生成待签名的字符串"""
+        accept = "" if headers.get("accept") is None else headers.get("accept")
+        content_md5 = "" if headers.get("content-md5") is None else headers.get("content-md5")
+        content_type = "" if headers.get("content-type") is None else headers.get("content-type")
+        date = "" if headers.get("date") is None else headers.get("date")
+        header = f"{method}\n{accept}\n{content_md5}\n{content_type}\n{date}\n"
+
+        canon_headers = self._get_canonicalized_headers(headers)
+        canon_resource = self._get_canonicalized_resource(url_path, query)
+        sign_str = header + canon_headers + canon_resource
+        return sign_str
+
+    def get_roasignature(self, string_to_sign, secret):
+        """生成签名: 使用HMAC-256生成签名, 然后通过base64输出签名字符串。"""
+        hash_val = hmac.new(secret.encode("utf-8"), string_to_sign.encode("utf-8"), hashlib.sha1).digest()
+        signature = base64.b64encode(hash_val).decode("utf-8")
+        return signature
+
+    def _get_canonicalized_headers(self, headers):
+        """将请求头排序后, 获取“x-acs-”作为前缀按字母序排序后拼接。
+        注意, 按RFC2616, HTTP 请求头的名称是大小写不敏感的。
+        """
+        canon_keys = []
+        for k in headers:
+            if k.startswith("x-acs-"):
+                canon_keys.append(k)
+        canon_keys = sorted(canon_keys)
+        canon_header = ""
+        for k in canon_keys:
+            canon_header += f"{k}:{ headers[k]}\n"
+        return canon_header
+
+    def do_request(self, api_product, api_query, api_method, api_path):
+        """根据请求信息，生成认证信息，发送请求给到后端服务"""
+        ts = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
+        signature_nonce = str(uuid.uuid4())
+        headers = {
+            "x-acs-signature-method": "HMAC-SHA1",
+            "date": ts,
+            "x-acs-signature-nonce": signature_nonce,
+            "x-pai-product": api_product,
+            "accept": "application/json",
+        }
+        api_url = f"{self.protocol}://{self.endpoint}{api_path}"
+        # 使用请求信息，生成请求使用的签名(signature)，然后生成对应认证信息，在请求头里传递给到服务(authorization)
+        string_to_sign = self.get_string_to_sign(method=api_method, url_path=api_path, headers=headers, query=api_query)
+        signature = self.get_roasignature(string_to_sign=string_to_sign, secret=self.access_key_secret)
+        headers["authorization"] = f"acs {self.access_key_id}:{signature}"
+        resp = requests.request(method=api_method, url=api_url, params=api_query, headers=headers, verify=False)
+
+        # print(resp.status_code)
+        # print(resp.content)
+
+        if resp.status_code != 200:
+            print(resp.text)
+            return None
+        return resp.json()
+
+    def do_get_job(self, jobid: str):
+        # API请求的URL，以及参数
+        api_path = f"/api/v1/jobs/{jobid}"
+        # 填写请求query上的参数
+        api_method = "GET"
+        api_product = "dlc"
+        api_query = {"jobid": jobid}
+
+        job_meta = self.do_request(
+            api_product=api_product, api_query=api_query, api_method=api_method, api_path=api_path
+        )
+
+        return job_meta
+
+    def get_node_pod(self, podmetalist):
+            nodelist = []
+            podlist = []
+
+            for podmeta in podmetalist:
+                nodelist.append(podmeta["NodeName"])
+                podlist.append(podmeta["PodId"])
+
+            return list(set(nodelist)), list(set(podlist))
 
 class NodeInfo:
     def __init__(self, hostname) -> None:
@@ -152,13 +283,13 @@ class ProcessFilter:
         printflock(f"RANK {self.rank} in HOST {self.nodeinfo.hostname} has outer busBw {self.nodeinfo.ib_bw} GB/s", flush=True)
         dist.destroy_process_group()
         
-        with open("tmp_nccltest.log", "a+") as f:
-            fcntl.flock(f, fcntl.LOCK_EX)
-            try:
-                if self.nodeinfo.ib_bw < self.ib_threshold and self.local_rank == 0:
+        if self.nodeinfo.ib_bw < self.ib_threshold and self.local_rank == 0:
+            with open("tmp_nccltest.log", "a+") as f:
+                fcntl.flock(f, fcntl.LOCK_EX)
+                try:
                     f.write(f"{self.nodeinfo.hostname} has busBw  {self.nodeinfo.ib_bw} GB/s \n")
-            finally:
-                fcntl.flock(f, fcntl.LOCK_UN)
+                finally:
+                    fcntl.flock(f, fcntl.LOCK_UN)
             
             
     def do_all_reduce_single_node(self):
@@ -213,10 +344,10 @@ class ProcessFilter:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="nccl test tools for finding slow nodes")
-    parser.add_argument("--launcher", type=str, help="launcher (slurm or torch)")
+    parser.add_argument("--launcher", type=str, default="slurm", help="launcher (slurm, k8s, torch)")
     parser.add_argument("--ib_threshold", type=float, default=18, help="IB bandwidth threshold")
     parser.add_argument("--nvlink_threshold", type=float, default=200, help="NVLink bandwidth threshold")
-    parser.add_argument("--buffersize", type=int, default=512*1014*1024, help="test tensor buffer size")
+    parser.add_argument("--buffersize", type=int, default=1024*1014*1024, help="test tensor buffer size")
     parser.add_argument("--port", type=int, default=29500, help="torch distributed port")
     parser.add_argument("--local", action="store_true", help="nccl test on per node")
     parser.add_argument("--warmup", type=int, default=5, help="warm-up iters")
@@ -226,7 +357,27 @@ if __name__ == "__main__":
     
     args = parser.parse_args()
     if args.gethost:
-        print(socket.gethostname())
+        if args.launcher == "k8s":
+            jobid = os.getenv("KUBERNETES_POD_NAME").split("-")[0]
+            conf = ConfigParser()
+            conf.read(os.getenv("DLC_CONFIG"))
+            dlcjob_info = DLCJobInfo(
+                conf["aliyun"]["endpoint"].replace('"', ''),
+                conf["user"]["access_id"].replace('"', ''),
+                conf["user"]["access_key"].replace('"', ''),
+                conf["aliyun"]["protocol"].replace('"', ''),
+            )
+            jobmeta=dlcjob_info.do_get_job(jobid)
+            nodelist, podlist = dlcjob_info.get_node_pod(jobmeta["Pods"])
+            nodeliststr = " ".join(nodelist)
+            with open("test_nodes.log", "w") as f:
+                fcntl.flock(f, fcntl.LOCK_EX)
+                try:
+                    f.write(f"{nodeliststr}")
+                finally:
+                    fcntl.flock(f, fcntl.LOCK_UN)
+        else:
+            print(socket.gethostname())
     else:
         filter = ProcessFilter(launcher=args.launcher, ib_threshold=args.ib_threshold, nvlink_threshold=args.nvlink_threshold, buffer_size=args.buffersize, port=args.port, is_local=args.local, warmup=args.warmup, iters=args.iters)
         
--- tools/nccl_test.sh
+++ tools/nccl_test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-launcher=slurm
+launcher=dlc
 partition=llm2
 
-gpus_per_node=1
+gpus_per_node=8
 nnodes=4
 master_port=12345
+
+dlc_path="/cpfs01/shared/public/dlc"
+dlc_config_path="/cpfs01/user/jiaopenglong/.dlc.config"
+env_path="/cpfs01/shared/public/llm/llm-env/llm-env-20230605"
+image="pjlab-wulan-acr-registry-vpc.cn-wulanchabu.cr.aliyuncs.com/pjlab-eflops/chenxun-st:llm-test"
+workspace_id="ws1so95hgb5kn6ja"
+pwd_path=$(pwd)
 
 slow_nodes=""
 
@@ -15,24 +22,73 @@ function do_slurm(){
   srun -p $partition -w $nodes -N $num_nodes --gpus-per-task 1 --ntasks-per-node=$gpus_per_node python find_slow_nodes.py --launcher slurm --port $master_port
 }
 
-function get_nodes_slurm(){
-    local nodestr=$(srun -p $partition -N $nnodes --gpus-per-task 1 --ntasks-per-node=$gpus_per_node bash -c 'python find_slow_nodes.py --launcher slurm --nodes $SLURM_NODELIST')
-    local nodearray=($(echo $nodestr | awk -F ' ' '{for(i=1; i<=NF; i++) print $i}'))
-    nodes=$(printf "%s," "${nodearray[@]}")
-    nodes=${nodes%,}
-}
-
 function do_torch(){
-    local master_addr=$1
-    torchrun --master_addr $master_addr --master_port $master_port --nnodes $nnodes --nproc_per_node $gpus_per_node find_slow_nodes.py --launcher torch --port $master_port
+  local master_addr=$1
+  torchrun --master_addr $master_addr --master_port $master_port --nnodes $nnodes --nproc_per_node $gpus_per_node find_slow_nodes.py --launcher torch --port $master_port
 }
 
-function nccl_test_slurm(){
+function do_dlc(){
+  local nodes=$1
+  local num_nodes=$2
+  local master_addr=$3
+  local cmd="source ${env_path} && \
+cd ${pwd_path} && \
+torchrun --master_addr \$MASTER_ADDR \
+--master_port $master_port --nnodes \$WORLD_SIZE \
+--nproc_per_node $gpus_per_node --node_rank \$RANK \
+find_slow_nodes.py --launcher torch --port $master_port"
+  ${dlc_path} --config ${dlc_config_path} create job --name "nccltest" --interactive \
+--kind PyTorchJob --node_names $nodes \
+--worker_count $num_nodes --worker_cpu 24 --worker_gpu $gpus_per_node \
+--worker_memory 100 --worker_image ${image} --workspace_id ${workspace_id} --worker_shared_memory 10Gi \
+--command "$cmd"
+}
+
+function get_nodes_slurm(){
+  if test -f tmp_nccltest.log ; then
+    rm -f tmp_nccltest.log
+  fi
+
+  local nodestr=$(srun -p $partition -N $nnodes --gpus-per-task 1 --ntasks-per-node=$gpus_per_node bash -c 'python find_slow_nodes.py --gethost')
+  local nodearray=($(echo $nodestr | awk -F ' ' '{for(i=1; i<=NF; i++) print $i}'))
+
+  nodes=$(printf "%s," "${nodearray[@]}")
+  nodes=${nodes%,}
+}
+
+function get_nodes_dlc(){
+  if test -f test_nodes.log ; then
+    rm -f test_nodes.log
+  fi
+
+  local cmd="source ${env_path} && \
+export DLC_CONFIG=$dlc_config_path && \
+cd ${pwd_path} && \
+python find_slow_nodes.py --launcher k8s --gethost"
+
+  ${dlc_path} --config ${dlc_config_path} create job --name gethostname --interactive \
+--kind PyTorchJob --worker_count $nnodes --worker_cpu 24 --worker_gpu $gpus_per_node \
+--worker_memory 100 --worker_image ${image} --workspace_id ${workspace_id} --worker_shared_memory 10Gi \
+--command "${cmd}"
+
+  local nodestr=$(cat test_nodes.log)
+  local nodearray=($(echo $nodestr | awk -F ' ' '{for(i=1; i<=NF; i++) print $i}'))
+  nodes=$(printf "%s," "${nodearray[@]}")
+  nodes=${nodes%,}
+  rm -f test_nodes.log
+}
+
+function nccl_test(){
   local nodestr=$1
-  nodearray=($(echo $nodes | awk -F ',' '{for(i=1; i<=NF; i++) print $i}'))
+  local nodearray=($(echo $nodestr | awk -F ',' '{for(i=1; i<=NF; i++) print $i}'))
   echo $nodestr
   local num_group=${#nodearray[@]}
-  do_slurm "$nodestr" "${num_group}"
+  if [[ $launcher == "slurm" ]] ; then
+    do_slurm "$nodestr" "${num_group}"
+  elif [[ $launcher == "dlc" ]] ; then
+    do_dlc "$nodestr" "${num_group}" "${nodearray[1]}"
+  fi
+
   if test -f tmp_nccltest.log ; then
     rm -f tmp_nccltest.log
     local middle=$((num_group / 2))
@@ -52,7 +108,7 @@ function nccl_test_slurm(){
     else
       local left_nodestr=$(printf "%s," "${left_group[@]}")
       local left_nodestr=${left_nodestr%,}
-      nccl_test_slurm "${left_nodestr}"
+      nccl_test "${left_nodestr}"
     fi
 
     if [[ $num_right_group -le 1 ]] ; then
@@ -60,12 +116,13 @@ function nccl_test_slurm(){
     else
       local right_nodestr=$(printf "%s," "${right_group[@]}")
       local right_nodestr=${right_nodestr%,}
-      nccl_test_slurm "${right_nodestr}"
+      nccl_test "${right_nodestr}"
     fi
   fi
 }
 
-get_nodes_slurm
-nccl_test_slurm "${nodes}"
+get_nodes_dlc
+echo $nodes
+nccl_test "${nodes}"
 
 echo $slow_nodes
