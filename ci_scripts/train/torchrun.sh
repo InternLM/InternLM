36bc402d07ab584a31b72e74ddd2f3e7a9fdbf6f
zachtzy <zachietu@gmail.com>
2023-08-02 14:46:47 +0800

    test: optimization of ci scripts(variables, test data cleaning, etc).

--- ci_scripts/train/torchrun.sh
+++ ci_scripts/train/torchrun.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
+set -x
 
-rm -rf $GITHUB_WORKSPACE/llm_ckpts/20
-srun -p llm -N 1 torchrun --nnodes=1 --nproc_per_node=8 --master_port=29501 train.py --config ./ci_scripts/train/ci_7B_sft.py --launcher "torch"
+[[ -n ${GITHUB_WORKSPACE} ]] || { echo "should set GITHUB_WORKSPACE first before ci, exit."; exit 1; }
+readonly CKPTS_PATH="$GITHUB_WORKSPACE/llm_ckpts"
+readonly CKPTS20_PATH="$GITHUB_WORKSPACE/llm_ckpts/20"
+readonly CKPTS_OUTPUT="${CKPTS20_PATH}/*.pt"
+expected_num=21
+exit_code=0
 
-file_dir="$GITHUB_WORKSPACE/llm_ckpts/20/*.pt"
 source ./ci_scripts/common/basic_func.sh
 
-num_files ${file_dir}
+echo "start to test torch training."
 
-if [ $file_num -ne 21 ]
-then
-    echo "The num of files is not right"
-    ls -l $file_dir
-    rm -rf $GITHUB_WORKSPACE/llm_ckpts
-    exit 1
+if [[ -d ${CKPTS20_PATH} ]]; then
+    if ! rm -rf ${CKPTS20_PATH}/*; then
+       echo "cleaning cached file in ${CKPTS20_PATH} failed, exit."
+       exit 1
+    fi
 fi
+
+srun -p llm2 -N 1 torchrun --nnodes=1 --nproc_per_node=8 --master_port=29501 train.py --config ./ci_scripts/train/ci_7B_sft.py --launcher torch
+[[ $? -ne 0 ]] && { echo "test torch training failed.";  exit_code=$(($exit_code + 1)); }
+
+num=$(num_files "${CKPTS_OUTPUT}")
+if [[ ${num} -ne ${expected_num} ]]; then
+    echo "expect: ${expected_num} files, actual: ${num} files."
+    exit_code=$(($exit_code + 1)) 
+fi
+
+# clean the test files.
+if ! rm -rf ${CKPTS_PATH}/*; then
+    echo "cleaning cached file in ${CKPTS_PATH} failed."
+    exit_code=$(($exit_code + 1))
+fi
+
+exit $exit_code
