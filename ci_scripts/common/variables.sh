3f52dd4d2689ba14ab4e2a20fce717633bdae599
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-03 15:07:39 +0800

    use default variables in ci_scripts

--- ci_scripts/common/variables.sh
+++ ci_scripts/common/variables.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# TODO: variable defination should be in repo configures. 
-readonly DATA_VOLUME=${DATA_VOLUME:-"/mnt/petrelfs/qa-caif-cicd/data"}
+readonly DATA_VOLUME=$(echo $GITHUB_WORKSPACE |cut -d '/' -f 1-4)/data
