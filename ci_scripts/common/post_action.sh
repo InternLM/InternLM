5ff39243b2dd1a078d0f62eb123868aed0326a0a
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-08 13:29:06 +0800

    performance echo

--- ci_scripts/common/post_action.sh
+++ ci_scripts/common/post_action.sh
@@ -4,14 +4,13 @@ set -x
 readonly JOB_LOG=$GITHUB_WORKSPACE/${GITHUB_JOB}.log
 
 if [[ ! -f ${JOB_LOG} ]]; then
-   echo "There is no ${JOB_LOG}. May be there is no job needed canceled"
+   echo "There is no ${JOB_LOG}. Maybe there is no job needed canceled"
    exit 0
 fi
 
 jobid=$(grep "queued and waiting" ${JOB_LOG} | grep -oP "\d+" || echo "6666")
-echo "jobid is ${jobid}"
 datetime=$(date '+%Y-%m-%d %H:%M:%S')
-echo "$datetime,The slurm job $jobid will be canceled"
+echo "$datetime,The slurm job $jobid will be canceled" | tr "\n" " "
 scancel $jobid
 # double cancel
 scancel $jobid
