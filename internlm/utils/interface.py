cf498c471e0cf969b625ed04093316c9d0fac392
Daniel-xsy <1491387884@qq.com>
2023-08-10 06:41:14 +0000

    Update script

--- internlm/utils/interface.py
+++ internlm/utils/interface.py
@@ -37,12 +37,12 @@ def generation_iterator(
     for k, v in inputs.items():
         inputs[k] = v.cuda()
     input_ids = inputs["input_ids"]
-    batch_size, input_ids_seq_length = input_ids.shape[0], input_ids.shape[-1]  # noqa: F841
+    input_ids_seq_length = input_ids.shape[-1]
     if generation_config is None:
         generation_config = model.generation_config
     generation_config = copy.deepcopy(generation_config)
     model_kwargs = generation_config.update(**kwargs)
-    bos_token_id, eos_token_id = generation_config.bos_token_id, generation_config.eos_token_id  # noqa: F841
+    eos_token_id = generation_config.eos_token_id
     if isinstance(eos_token_id, int):
         eos_token_id = [eos_token_id]
     if additional_eos_token_id is not None:
@@ -58,20 +58,24 @@ def generation_iterator(
     elif generation_config.max_new_tokens is not None:
         generation_config.max_length = generation_config.max_new_tokens + input_ids_seq_length
         if not has_default_max_length:
-            logger.warn(
-                f"Both `max_new_tokens` (={generation_config.max_new_tokens}) and `max_length`(="
-                f"{generation_config.max_length}) seem to have been set. `max_new_tokens` will take precedence. "
+            logger.warning(
+                "Both `max_new_tokens` (={%s}) and `max_length`(="
+                "{%s}) seem to have been set. `max_new_tokens` will take precedence. "
                 "Please refer to the documentation for more information. "
                 "(https://huggingface.co/docs/transformers/main/en/main_classes/text_generation)",
-                UserWarning,
+                generation_config.max_new_tokens,
+                generation_config.max_length,
             )
 
     if input_ids_seq_length >= generation_config.max_length:
         input_ids_string = "input_ids"
         logger.warning(
-            f"Input length of {input_ids_string} is {input_ids_seq_length}, but `max_length` is set to"
-            f" {generation_config.max_length}. This can lead to unexpected behavior. You should consider"
-            " increasing `max_new_tokens`."
+            "Input length of {%s} is {%s}, but `max_length` is set to"
+            " {%s}. This can lead to unexpected behavior. You should consider"
+            " increasing `max_new_tokens`.",
+            input_ids_string,
+            input_ids_seq_length,
+            generation_config.max_length,
         )
 
     # 2. Set generation parameters if not already defined
