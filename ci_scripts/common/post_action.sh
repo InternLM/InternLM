ae6258b4b5d5788123bb46d58b8414fa8a3188e0
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-10 14:24:13 +0800

    litmit retry times

--- ci_scripts/common/post_action.sh
+++ ci_scripts/common/post_action.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -x
 
-cancel_slurm_job() {
-jobid=$(squeue -o "%A %j" -p llm2 -u $USER | grep $GITHUB_RUN_ID-$GITHUB_JOB | awk '{print $1}')
-if [ -n "$jobid" ];then
-   echo "The job $jobid will be canceled"
-   scancel $jobid
-   sleep 0.1
-   cancel_slurm_job   
-else
-   echo "There has been no job needed canceled"
-fi
-}
+retry_times=3
+for ((i=1;i<=$retry_times;i++))
+do
+    jobid=$(squeue -o "%A %j" -p llm2 -u $USER | grep $GITHUB_RUN_ID-$GITHUB_JOB | awk '{print $1}')
+    if [[ -n "$jobid" ]];then
+        echo "The job $jobid will be canceled."
+        scancel $jobid
+        sleep 0.5
+    else
+        echo "There are no more jobs that need to be canceled."
+        break
+    fi
 
-cancel_slurm_job
+    if [[ $i -eq $retry_times ]];then
+        echo "There have been tried $retry_times times. Please contact user $USER to confirm the job status."
+    fi
+done
 
 exit 0
