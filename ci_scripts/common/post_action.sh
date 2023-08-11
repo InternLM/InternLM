fcb339ae73853160fb589867a08061e5817da8fb
qa-caif-cicd <qa-caif-cicd@pjlab.org.cn>
2023-08-11 10:03:17 +0800

    add {} for variables

--- ci_scripts/common/post_action.sh
+++ ci_scripts/common/post_action.sh
@@ -3,7 +3,7 @@ set -x
 
 retry_times=3
 for ((i=1;i<=$retry_times;i++));do
-    jobid=$(squeue -o "%A %j" -u $USER | grep $GITHUB_RUN_ID-$GITHUB_JOB | awk '{print $1}')
+    jobid=$(squeue -o "%A %j" -u $USER | grep ${GITHUB_RUN_ID}-${GITHUB_JOB} | awk '{print $1}')
     if [[ -n "$jobid" ]];then
         echo "The job $jobid will be canceled."
         scancel $jobid
