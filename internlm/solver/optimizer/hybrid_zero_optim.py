4fb9d8907e6c10b3d7e57140b3dee3b8fbb2da55
yingtongxiong <974106207@qq.com>
2023-08-30 19:26:21 +0800

    modify the dtype and padding tensor in optimizer

--- internlm/solver/optimizer/hybrid_zero_optim.py
+++ internlm/solver/optimizer/hybrid_zero_optim.py
@@ -110,7 +110,7 @@ class HybridZeroOptimizer(BaseOptimizer):
 
         super().__init__(optim=optimizer)
 
-        self._dtype = self.optim.param_groups[0]["params"][0].dtype #TODO there are two param groups
+        self._dtype = dict()
         self._cpu_offload = cpu_offload
         self._zero_local_rank = gpc.get_local_rank(ParallelMode.ZERO1)
         self._zero_world_size = gpc.get_world_size(ParallelMode.ZERO1)
@@ -148,8 +148,8 @@ class HybridZeroOptimizer(BaseOptimizer):
         # need to record the rank in which parameter groups are not assigned parameters.
         self.param_group_has_params = []
         self.param_group_no_params_ranks = []
-        self.padding_grad = torch.zeros([32], dtype=self._dtype, device=get_current_device())
-        self.padding_tensor = torch.zeros([32], dtype=self._dtype, device=get_current_device())
+        self.padding_grad = dict()
+        self.padding_tensor = dict()
 
         self.rank_unique_id = (
             f"gpus-{gpc.get_world_size(ParallelMode.GLOBAL)}_"
@@ -170,6 +170,10 @@ class HybridZeroOptimizer(BaseOptimizer):
         # and add buffers to parameter store for future access
         for group_id, param_group in enumerate(self.optim.param_groups):
             group_params = param_group["params"]
+            
+            self._dtype[group_id] = group_params[0].dtype
+            self.padding_grad[group_id] = torch.zeros([32], dtype=self._dtype[group_id], device=get_current_device())
+            self.padding_tensor[group_id] = torch.zeros([32], dtype=self._dtype[group_id], device=get_current_device())
 
             # add the fp16 params to fp16_param_groups for bookkeeping
             self._fp16_param_groups[group_id] = group_params
@@ -403,7 +407,6 @@ class HybridZeroOptimizer(BaseOptimizer):
             flat = bucket.flatten()
             reduced_flat = reduce_tensor(
                 tensor=flat,
-                dtype=self.dtype, #TODO should change
                 dst_rank=reduce_rank,
                 parallel_mode=ParallelMode.DATA,
             )
@@ -490,8 +493,8 @@ class HybridZeroOptimizer(BaseOptimizer):
         # compute norm for gradients that have been reduced
         params, grads = self._param_store.get_reduced_param_for_compute_norm(group_id=group_id, last_bucket=last_bucket)
         if len(params) == 0:
-            grads = [self.padding_grad]
-            params = [self.padding_tensor]
+            grads = [self.padding_grad[group_id]]
+            params = [self.padding_tensor[group_id]]
 
         if self._clip_grad_norm > 0:
             # this norm is before scaling, it will be very large
