c5f0803b04aa22d349d30fb640fbd6e135e2a7ba
yingtongxiong <974106207@qq.com>
2023-09-01 10:57:24 +0800

    add name for param groups

--- internlm/train/training_internlm.py
+++ internlm/train/training_internlm.py
@@ -100,8 +100,8 @@ def initialize_optimizer(model: Union[nn.Module, nn.ModuleList]):
     adam_cfg = gpc.config.adam
     naive_optimizer = torch.optim.AdamW(
         params=[
-            {"params": model.not_norm, "weight_decay": adam_cfg.weight_decay},
-            {"params": model.norm, "weight_decay": adam_cfg.weight_decay},
+            {"params": model.not_norm, "weight_decay": adam_cfg.weight_decay, "name": "default"},
+            {"params": model.norm, "weight_decay": adam_cfg.weight_decay, "name": "norm"},
         ],
         lr=adam_cfg.lr,
         betas=(adam_cfg.adam_beta1, adam_cfg.adam_beta2),
