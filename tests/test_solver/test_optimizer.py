50b866cd01a4ebd6c3d5ea00fc9d877432ac512b
li126com <li126com2@126.com>
2023-09-12 19:39:52 +0800

    feat: add optimizer test

--- tests/test_solver/test_optimizer.py
+++ tests/test_solver/test_optimizer.py
@@ -1,24 +1,23 @@
-import pytest
 import copy
-import torch
-import torch.nn as nn
-from torch.nn.parallel import DistributedDataParallel as DDP
-import torch.distributed as dist
+import multiprocessing as mp
+import random
 
+import numpy as np
+import pytest
+import torch
+from torch import nn
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.testing import assert_close
+
+import internlm
 from internlm.core.context.parallel_context import Config
 from internlm.solver.optimizer import HybridZeroOptimizer
 from internlm.solver.optimizer.utils import ParamBcastSyncHandler
-import random
-import numpy as np
-import torch
-from torch.testing import assert_close
-import internlm
-import multiprocessing as mp
 
 
 class MlpModel(nn.Module):
     def __init__(self):
-        super(MlpModel, self).__init__()
+        super().__init__()
         self.linear1 = nn.Linear(128, 256)
         self.linear2 = nn.Linear(256, 512)
 
@@ -26,6 +25,7 @@ class MlpModel(nn.Module):
         x = self.linear1(x)
         x = self.linear2(x)
         return x
+
 
 config = Config(
     dict(
@@ -54,7 +54,7 @@ config = Config(
         alert_address=None,
         monitor=dict(alert=dict(enable_feishu_alert=False, feishu_alert_address=None, light_monitor_address=None)),
         grad_scaler=dict(
-                fp16=dict(
+            fp16=dict(
                 initial_scale=1,
                 min_scale=1,
                 growth_interval=1,
@@ -64,7 +64,7 @@ config = Config(
             max_scale=1,
             hysteresis=1,
         ),
-        adam = dict(
+        adam=dict(
             lr=1e-4,
             adam_beta1=0.9,
             adam_beta2=0.95,
@@ -72,28 +72,26 @@ config = Config(
             adam_eps=1e-8,
             weight_decay=0.01,
         ),
-        hybrid_zero_optimizer = dict(
-            # Enable low_level_optimzer overlap_communication
+        hybrid_zero_optimizer=dict(
             overlap_sync_grad=True,
             overlap_sync_param=True,
-            # bucket size for nccl communication params
             reduce_bucket_size=512 * 1024 * 1024,
-            # grad clipping
             clip_grad_norm=1.0,
-        )
+        ),
     )
-)    
+)
 
 
 def build_environment(rank, world_size):
     import os
-    os.environ['RANK'] = str(rank)
+
+    os.environ["RANK"] = str(rank)
     os.environ["LOCAL_RANK"] = str(rank)
     os.environ["WORLD_SIZE"] = str(world_size)
     os.environ["MASTER_ADDR"] = "127.0.0.1"
     os.environ["MASTER_PORT"] = "12345"
     torch.cuda.empty_cache()
-    #launcher="torch"
+    # launcher="torch"
     internlm.launch_from_torch(config=config, seed=1024)
 
 
@@ -104,16 +102,16 @@ def seed_all(seed, cuda_deterministic=False):
     if torch.cuda.is_available():
         torch.cuda.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-    if cuda_deterministic:    # slower, more reproducible
+    if cuda_deterministic:  # slower, more reproducible
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
     else:
         torch.backends.cudnn.deterministic = False
         torch.backends.cudnn.benchmark = True
-        
+
 
 def loose_close(a, b, dtype: torch.dtype = torch.float32):
-    rtol, atol = (1e-3, 5e-3) 
+    rtol, atol = (1e-3, 5e-3)
     if dtype is torch.float16:
         rtol = 5e-2
         atol = 5e-4
@@ -122,16 +120,6 @@ def loose_close(a, b, dtype: torch.dtype = torch.float32):
         atol = 4e-3
 
     assert_close(a, b, rtol=rtol, atol=atol)
-    
-
-def split_ddp_grad(grad, world_size):
-    with torch.no_grad():
-        grad = grad.clone().detach().flatten()
-        padding_size = (world_size - grad.numel() % world_size) % world_size
-        if padding_size > 0:
-            grad = torch.nn.functional.pad(grad, [0, padding_size])
-        splited_grad = grad.split(grad.numel() // world_size)
-    return splited_grad
 
 
 def exam_hybrid_zero_optim_with_ddp(args):
@@ -141,7 +129,7 @@ def exam_hybrid_zero_optim_with_ddp(args):
     config.hybrid_zero_optimizer.overlap_sync_param = overlap_sync_param
     config.hybrid_zero_optimizer.overlap_sync_grad = overlap_sync_grad
     config.data.micro_num = micro_num
-    
+
     build_environment(rank, world_size)
 
     # fix seed
@@ -150,15 +138,15 @@ def exam_hybrid_zero_optim_with_ddp(args):
     # create models
     zero_model = MlpModel().cuda()
     torch_model = copy.deepcopy(zero_model)
-    
+
     torch_model = DDP(torch_model.cuda(), static_graph=True).cuda()
-    
+
     # create optimizer
     if config.hybrid_zero_optimizer.overlap_sync_param:
         param_bcast_sync_handler = ParamBcastSyncHandler(zero_model)
     else:
         param_bcast_sync_handler = None
-        
+
     naive_optimizer = torch.optim.AdamW(
         params=[{"params": zero_model.parameters(), "weight_decay": config.adam.weight_decay}],
         lr=config.adam.lr,
@@ -172,41 +160,51 @@ def exam_hybrid_zero_optim_with_ddp(args):
         zero_cfg=config.hybrid_zero_optimizer,
         param_bcast_sync_handler=param_bcast_sync_handler,
     )
-    
+
     torch_optimizer = torch.optim.AdamW(
         params=[{"params": torch_model.parameters(), "weight_decay": config.adam.weight_decay}],
         lr=config.adam.lr,
         betas=(config.adam.adam_beta1, config.adam.adam_beta2),
         eps=config.adam.adam_eps,
     )
-    
-    # create input
-    input_data = torch.rand(16, 128).cuda()
-    
-    # zero-dp forward
-    zero_output = zero_model(input_data)
 
-    # torch-ddp forward
-    torch_output = torch_model(input_data)
-    loose_close(zero_output, torch_output)
-    
-    # zero-dp backward
-    zero_optimizer.backward(zero_output.mean())
+    for _ in range(5):
+        zero_optimizer.zero_grad()
+        torch_optimizer.zero_grad()
+        zero_optimizer.skip_grad_reduce = True
+        for num in range(micro_num):
+            if num == micro_num - 1:
+                zero_optimizer.skip_grad_reduce = False
+            # create input
+            input_data = torch.rand(16, 128).cuda()
 
-    # torch-ddp backward
-    torch_output.mean().backward()
-    
-    # check grad
-    for torch_parm, zero_parm in zip(torch_model.parameters(), zero_model.parameters()):
-        if torch_parm.grad is not None:
-            loose_close(torch_parm.grad, zero_parm.grad)
-                
-    # zero-dp step
-    zero_optimizer.step()
+            # zero-dp forward
+            zero_output = zero_model(input_data)
 
-    # torch ddp step
-    torch_optimizer.step()
+            # torch-ddp forward
+            torch_output = torch_model(input_data)
 
+            # check output
+            loose_close(zero_output, torch_output)
+
+            # zero-dp backward
+            zero_optimizer.backward(zero_output.mean())
+
+            # torch-ddp backward
+            torch_output.mean().backward()
+
+        # check grad
+        for torch_parm, zero_parm in zip(torch_model.parameters(), zero_model.parameters()):
+            if torch_parm.grad is not None:
+                loose_close(torch_parm.grad, zero_parm.grad)
+
+        # zero-dp step
+        zero_optimizer.step()
+
+        # torch-ddp step
+        torch_optimizer.step()
+
+    torch.cuda.synchronize()
     # check updated param
     for torch_parm, zero_parm in zip(torch_model.parameters(), zero_model.parameters()):
         loose_close(torch_parm, zero_parm)
@@ -214,11 +212,9 @@ def exam_hybrid_zero_optim_with_ddp(args):
 
 def exam_hybrid_zero_optim_with_ckpt_load_save(args):
     # init
-    rank, world_size, zero_parallel, overlap_sync_param, overlap_sync_grad= args
+    rank, world_size, zero_parallel = args
     config.parallel.zero1 = zero_parallel
-    config.hybrid_zero_optimizer.overlap_sync_param = overlap_sync_param
-    config.hybrid_zero_optimizer.overlap_sync_grad = overlap_sync_grad
-    
+
     build_environment(rank, world_size)
 
     # fix seed
@@ -226,13 +222,13 @@ def exam_hybrid_zero_optim_with_ckpt_load_save(args):
 
     # create models
     zero_model = MlpModel().cuda()
-    
+
     # create optimizer
     if config.hybrid_zero_optimizer.overlap_sync_param:
         param_bcast_sync_handler = ParamBcastSyncHandler(zero_model)
     else:
         param_bcast_sync_handler = None
-        
+
     naive_optimizer = torch.optim.AdamW(
         params=[{"params": zero_model.parameters(), "weight_decay": config.adam.weight_decay}],
         lr=config.adam.lr,
@@ -246,47 +242,45 @@ def exam_hybrid_zero_optim_with_ckpt_load_save(args):
         zero_cfg=config.hybrid_zero_optimizer,
         param_bcast_sync_handler=param_bcast_sync_handler,
     )
-    
+
     naive_optimizer2 = torch.optim.AdamW(
         params=[{"params": zero_model.parameters(), "weight_decay": config.adam.weight_decay}],
         lr=config.adam.lr,
         betas=(config.adam.adam_beta1, config.adam.adam_beta2),
         eps=config.adam.adam_eps,
     )
-    
+
     zero_optimizer2 = HybridZeroOptimizer(
         naive_optimizer2,
         grad_scal_cfg=config.grad_scaler,
         zero_cfg=config.hybrid_zero_optimizer,
         param_bcast_sync_handler=param_bcast_sync_handler,
     )
-    
+
     # save and load states
     states = zero_optimizer.state_dict()
     zero_optimizer2.load_state_dict(states)
-    
+
     # check fp32 model weights
-    for zero1_param, zero2_param in \
-        zip(zero_optimizer._fp32_flat_param_groups_of_current_rank.values(), \
-            zero_optimizer2._fp32_flat_param_groups_of_current_rank.values()):
-            loose_close(zero1_param, zero2_param)
-    
+    for zero1_param, zero2_param in zip(
+        zero_optimizer._fp32_flat_param_groups_of_current_rank.values(),
+        zero_optimizer2._fp32_flat_param_groups_of_current_rank.values(),
+    ):
+        loose_close(zero1_param, zero2_param)
+
     # check fp16 model weights
-    for zero1_param, zero2_param in \
-        zip(zero_optimizer._fp16_param_groups.values(), \
-            zero_optimizer2._fp16_param_groups.values()):
-            loose_close(zero1_param, zero2_param)
-    
-    
+    for zero1_param, zero2_param in zip(
+        zero_optimizer._fp16_param_groups.values(), zero_optimizer2._fp16_param_groups.values()
+    ):
+        loose_close(zero1_param, zero2_param)
 
-zero_parallel_check_list = [-1, 1, 4] #[-1, 1, 4]
+
+zero_parallel_check_list = [-1, 1, 4]
 overlap_sync_grad_check_list = [True, False]
-overlap_sync_param_check_list = [False] #[True, False]
-miro_num_check_list = [1, 2, 4] #[1, 2, 4]
+overlap_sync_param_check_list = [True, False]
+miro_num_check_list = [1, 2, 4]
 
-# 首先需要实现一个正确的 pytorch ddp，并且要支持 no_sync
-# 先不考虑带 tp 和 pp 的情况
-# case1: 基本正确性测试，单机8卡
+
 @pytest.mark.parametrize("zero_parallel", zero_parallel_check_list)
 @pytest.mark.parametrize("overlap_sync_param", overlap_sync_param_check_list)
 @pytest.mark.parametrize("overlap_sync_grad", overlap_sync_grad_check_list)
@@ -294,24 +288,22 @@ miro_num_check_list = [1, 2, 4] #[1, 2, 4]
 def test_hybrid_zero_optim_with_ddp(zero_parallel, overlap_sync_param, overlap_sync_grad, micro_num):
     ctx = mp.get_context("spawn")
     with ctx.Pool(processes=8) as pool:
-        pool.map(exam_hybrid_zero_optim_with_ddp, [[rank, 8, zero_parallel, overlap_sync_param, 
-                                                    overlap_sync_grad, micro_num] for rank in range(8)])
+        pool.map(
+            exam_hybrid_zero_optim_with_ddp,
+            [[rank, 8, zero_parallel, overlap_sync_param, overlap_sync_grad, micro_num] for rank in range(8)],
+        )
         pool.close()
         pool.join()
 
 
-# # case2: ckpt保存和加载可以完全恢复之前的状态
 @pytest.mark.parametrize("zero_parallel", zero_parallel_check_list)
-@pytest.mark.parametrize("overlap_sync_param", overlap_sync_param_check_list)
-@pytest.mark.parametrize("overlap_sync_grad", overlap_sync_grad_check_list)
-def test_hybrid_zero_optim_with_ckpt_load_save(zero_parallel, overlap_sync_param, overlap_sync_grad):
+def test_hybrid_zero_optim_with_ckpt_load_save(zero_parallel):
     ctx = mp.get_context("spawn")
     with ctx.Pool(processes=8) as pool:
-        pool.map(exam_hybrid_zero_optim_with_ckpt_load_save, [[rank, 8, zero_parallel, overlap_sync_param, 
-                                                                overlap_sync_grad] for rank in range(8)])
+        pool.map(exam_hybrid_zero_optim_with_ckpt_load_save, [[rank, 8, zero_parallel] for rank in range(8)])
         pool.close()
         pool.join()
 
 
-if __name__ == '__main__':
-    pytest.main(['-s', '-q', 'test_optimizer.py']) #'-m embedding',
+if __name__ == "__main__":
+    pytest.main(["-s", "-q", "test_optimizer.py"])
