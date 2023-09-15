28294e1ee3093ba612a3de2a4268fc35f2c9bd5d
x54-729 <17307130121@fudan.edu.cn>
2023-09-15 20:32:09 +0800

    delete unnecessary print; fix model card typo; fix chat epoch

--- tools/openai_api.py
+++ tools/openai_api.py
@@ -1,17 +1,18 @@
 import time
-import torch
-import uvicorn
-from pydantic import BaseModel, Field
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from typing import List, Literal, Optional, Union
-from transformers import AutoTokenizer, AutoModelForCausalLM
+
+import torch
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
 from sse_starlette.sse import EventSourceResponse
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI): # collects GPU memory
+async def lifespan(app: FastAPI):  # collects GPU memory
     yield
     if torch.cuda.is_available():
         torch.cuda.empty_cache()
@@ -27,6 +28,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 class ModelCard(BaseModel):
     id: str
@@ -83,7 +85,7 @@ class ChatCompletionResponse(BaseModel):
 
 @app.get("/internlm/models", response_model=ModelList)
 async def list_models():
-    model_card = ModelCard(id="gpt-3.5-turbo")
+    model_card = ModelCard(id="internlm")
     return ModelList(data=[model_card])
 
 
@@ -102,8 +104,8 @@ async def create_chat_completion(request: ChatCompletionRequest):
     history = []
     if len(prev_messages) % 2 == 0:
         for i in range(0, len(prev_messages), 2):
-            if prev_messages[i].role == "user" and prev_messages[i+1].role == "assistant":
-                history.append([prev_messages[i].content, prev_messages[i+1].content])
+            if prev_messages[i].role == "user" and prev_messages[i + 1].role == "assistant":
+                history.append([prev_messages[i].content, prev_messages[i + 1].content])
 
     if request.stream:
         generate = predict(query, history, request.model)
@@ -111,9 +113,7 @@ async def create_chat_completion(request: ChatCompletionRequest):
 
     response, _ = model.chat(tokenizer, query, history=history)
     choice_data = ChatCompletionResponseChoice(
-        index=0,
-        message=ChatMessage(role="assistant", content=response),
-        finish_reason="stop"
+        index=0, message=ChatMessage(role="assistant", content=response), finish_reason="stop"
     )
 
     return ChatCompletionResponse(model=request.model, choices=[choice_data], object="chat.completion")
@@ -122,46 +122,30 @@ async def create_chat_completion(request: ChatCompletionRequest):
 async def predict(query: str, history: List[List[str]], model_id: str):
     global model, tokenizer
 
-    choice_data = ChatCompletionResponseStreamChoice(
-        index=0,
-        delta=DeltaMessage(role="assistant"),
-        finish_reason=None
-    )
+    choice_data = ChatCompletionResponseStreamChoice(index=0, delta=DeltaMessage(role="assistant"), finish_reason=None)
     chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
     yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
 
     current_length = 0
 
-    for new_response in model.stream_chat(tokenizer, query, history):
-        # skip history
-        if isinstance(new_response, list):
-            continue
+    for new_response, _ in model.stream_chat(tokenizer, query, history):
         if len(new_response) == current_length:
             continue
-        print(new_response, type(new_response))
 
         new_text = new_response[current_length:]
 
         current_length = len(new_response)
 
         choice_data = ChatCompletionResponseStreamChoice(
-            index=0,
-            delta=DeltaMessage(content=new_text),
-            finish_reason=None
+            index=0, delta=DeltaMessage(content=new_text), finish_reason=None
         )
         chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
         yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
 
-
-    choice_data = ChatCompletionResponseStreamChoice(
-        index=0,
-        delta=DeltaMessage(),
-        finish_reason="stop"
-    )
+    choice_data = ChatCompletionResponseStreamChoice(index=0, delta=DeltaMessage(), finish_reason="stop")
     chunk = ChatCompletionResponse(model=model_id, choices=[choice_data], object="chat.completion.chunk")
     yield "{}".format(chunk.json(exclude_unset=True, ensure_ascii=False))
-    yield '[DONE]'
-
+    yield "[DONE]"
 
 
 if __name__ == "__main__":
@@ -170,4 +154,4 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True)
     model.eval()
 
-    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
