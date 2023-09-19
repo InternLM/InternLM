299ade4d93611726687462a94fcf9f5de5c74c9a
Qu Wenwen <vinny_qu@163.com>
2023-09-19 12:45:56 +0800

    refactor code

--- internlm/train/utils.py
+++ internlm/train/utils.py
@@ -22,32 +22,36 @@ def split_params_into_different_groups_for_optimizer(param_groups: Tuple[Dict]) 
     elif not isinstance(param_groups, list):
         raise ValueError(f"Unknown param group type of {type(param_groups)}")
 
-    # Create fp32 and moe groups and copy origin attribute
-    for group_param in param_groups:
-        fp32_group = {}
+    new_groups = {}
+    for pgroup in param_groups:
+        new_groups[pgroup["name"]] = {}
+        new_groups[pgroup["name"]]["fp32"] = {}
 
+        # Create fp32 groups and copy origin attribute
+        fp32_group = new_groups[pgroup["name"]]["fp32"]
+        fp32_group["name"] = pgroup["name"] + "_fp32"
         # copy attribute for fp32 group
-        for ori_key in group_param.keys():
-            if ori_key == "name":
-                fp32_group["name"] = ori_key + "_fp32"
-            else:
+        for ori_key in pgroup.keys():
+            if ori_key != "name":
                 if ori_key == "params":
                     fp32_group[ori_key] = []
                 else:
-                    fp32_group[ori_key] = group_param[ori_key]
+                    fp32_group[ori_key] = pgroup[ori_key]
 
         # Assign param
-        new_params = []
-        for param in group_param["params"]:
+        origin_params = []
+        for param in pgroup["params"]:
             if param.dtype == torch.float32:
                 fp32_group["params"].append(param)
             else:
-                new_params.append(param)
+                origin_params.append(param)
 
         # origin group without fp32
-        group_param["params"] = new_params
-        # append to origin group
-        param_groups.append(fp32_group)
+        pgroup["params"] = origin_params
+
+    for _, v in new_groups.items():
+        for _, v1 in v.items():
+            param_groups.append(v1)
 
     return tuple(param_groups)
 
