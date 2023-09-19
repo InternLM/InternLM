bfefc4ea3c321afa25d6c4c4591f16f62c3d772c
kkscilife <126147887+kkscilife@users.noreply.github.com>
2023-09-19 14:52:32 +0800

    test(ci_scripts): move ci env (#317)
    * change partition and runner label
    
    * change rm action to mv
    
    * use spot
    
    * use rsync to move test files
    
    * remove *
    
    * remove *
    
    * change into llm_s partition
    
    ---------
    
    Co-authored-by: wangmengke <wangmengke@pjlab.org.cn>

--- ci_scripts/data/tokenizer_chinese.sh
+++ ci_scripts/data/tokenizer_chinese.sh
@@ -2,7 +2,8 @@
 set -x
 
 source ./ci_scripts/common/variables.sh
-[[ -n ${DATA_VOLUME} ]] || { echo "should set DATA_VOLUME first before ci."; exit 1; }
+[[ -n ${DATA_VOLUME} ]] || { echo "should set DATA_VOLUME first before ci, exit."; exit 1; }
+[[ -n ${CLEAN_PATH} ]] || { echo "should set CLEAN_PATH first before ci, exit."; exit 1; }
 
 readonly DATA=${DATA_VOLUME}/lm_data/cn_data/raw_data.txt
 readonly RESULT=${DATA_VOLUME}/lm_data/cn_data/result.bin
@@ -16,13 +17,13 @@ echo "start to test tokenizer.py."
 
 num=$(num_files "${RESULTS}")
 if [[ ${num} -gt 0 ]]; then
-    if ! rm -rf ${RESULTS}; then
+    if ! rsync -av --remove-source-files ${RESULTS} ${CLEAN_PATH}; then
        echo "cleaning test data ${RESULTS} failed, exit."
        exit 1
     fi
 fi
 
-srun -p ${SLURM_PARTITION} --job-name=$1 --gpus-per-task=1 python tools/tokenizer.py --text_input_path ${DATA} --bin_output_path ${RESULT}
+srun -p ${SLURM_PARTITION} --quotatype=spot --job-name=$1 --gpus-per-task=1 python tools/tokenizer.py --text_input_path ${DATA} --bin_output_path ${RESULT}
 [[ $? -ne 0 ]] && { echo "test tokenizer.py failed.";  exit_code=$(($exit_code + 1)); }
 
 file_list=($RESULT $RESULT_META)
@@ -33,8 +34,8 @@ for file in ${file_list[@]}; do
     fi
 done
 
-# clean the test files.
-if ! rm -rf ${RESULTS}/*; then
+# move the test files.
+if ! rsync -av --remove-source-files ${RESULTS} ${CLEAN_PATH}; then
    echo "cleaning cached file in ${RESULTS} failed."
    exit_code=$(($exit_code + 1))
 fi
--- ci_scripts/train/torchrun.sh
+++ ci_scripts/train/torchrun.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -x
 
+source ./ci_scripts/common/variables.sh
 [[ -n ${GITHUB_WORKSPACE} ]] || { echo "should set GITHUB_WORKSPACE first before ci, exit."; exit 1; }
+[[ -n ${CLEAN_PATH} ]] || { echo "should set CLEAN_PATH first before ci, exit."; exit 1; }
+
 readonly CKPTS_PATH="$GITHUB_WORKSPACE/llm_ckpts"
 readonly CKPTS20_PATH="$GITHUB_WORKSPACE/llm_ckpts/20"
 readonly CKPTS_OUTPUT="${CKPTS20_PATH}/*.pt"
@@ -13,13 +16,13 @@ source ./ci_scripts/common/basic_func.sh
 echo "start to test torch training."
 
 if [[ -d ${CKPTS20_PATH} ]]; then
-    if ! rm -rf ${CKPTS20_PATH}/*; then
+    if ! rsync -av --remove-source-files ${CKPTS20_PATH} ${CLEAN_PATH}; then
        echo "cleaning cached file in ${CKPTS20_PATH} failed, exit."
        exit 1
     fi
 fi
 
-srun -p ${SLURM_PARTITION} --exclusive --job-name=$1 -N 1 torchrun --nnodes=1 --nproc_per_node=8 --master_port=29501 train.py --config ./ci_scripts/train/ci_7B_sft.py --launcher torch
+srun -p ${SLURM_PARTITION} --exclusive --quotatype=spot --job-name=$1 -N 1 torchrun --nnodes=1 --nproc_per_node=8 --master_port=29501 train.py --config ./ci_scripts/train/ci_7B_sft.py --launcher torch
 [[ $? -ne 0 ]] && { echo "test torch training failed.";  exit_code=$(($exit_code + 1)); }
 
 num=$(num_files "${CKPTS_OUTPUT}")
@@ -28,8 +31,8 @@ if [[ ${num} -ne ${expected_num} ]]; then
     exit_code=$(($exit_code + 1))
 fi
 
-# clean the test files.
-if ! rm -rf ${CKPTS_PATH}/*; then
+# move the test files.
+if ! rsync -av --remove-source-files ${CKPTS_PATH}/* ${CLEAN_PATH}; then
     echo "cleaning cached file in ${CKPTS_PATH} failed."
     exit_code=$(($exit_code + 1))
 fi
