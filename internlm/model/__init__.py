1986116527b10ae348711be2b6a7daff136adf82
zhanglei <ryancheung98@163.com>
2023-09-25 15:36:35 +0800

    add moe module to `__init__.py`

--- internlm/model/__init__.py
+++ internlm/model/__init__.py
@@ -5,12 +5,14 @@ from .embedding import Embedding1D, RotaryEmbedding
 from .linear import FeedForward, RewardModelLinear, ScaleColumnParallelLinear
 from .metrics import AccPerplex
 from .modeling_internlm import build_model_with_cfg
+from .moe import MoE
 from .multi_head_attention import MHA
 from .utils import gather_forward_split_backward
 
 __all__ = [
     "Embedding1D",
     "FeedForward",
+    "MoE",
     "RotaryEmbedding",
     "RewardModelLinear",
     "ScaleColumnParallelLinear",
