2520edb7956783eb6844a22d81121d07d25f2ea7
gaoyang07 <Gary1546308416AL@gmail.com>
2023-10-27 10:20:43 +0800

    use try-except to handle file error

--- internlm/utils/storage_manager.py
+++ internlm/utils/storage_manager.py
@@ -512,10 +512,11 @@ class LocalClient(StorageClient):
     def sync_upload_fileobj(fp: str, saved_obj=None, **kwargs):
         assert saved_obj is not None
         fp_dirname = os.path.dirname(fp)
-        if gpc.get_global_rank() == 0:
+        try:
             if not os.path.exists(fp_dirname):
                 os.makedirs(fp_dirname, exist_ok=True)
-        dist.barrier()
+        except:
+            pass
         torch.save(saved_obj, fp, **kwargs)
 
     @staticmethod
