c5d073934e024213d8161cf3fb0364664deba62f
kkscilife <wangmengke@pjlab.org.cn>
2024-01-23 10:05:44 +0800

    add notify

--- tests/test_hf_model.py
+++ tests/test_hf_model.py
@@ -1,12 +1,9 @@
 import pytest
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from transformers import AutoTokenizer, AutoModelForCausalLM
+prompts = ["你好", "what's your name"]
 
-prompts = [
-  "你好",
-  "what's your name"
-]
 
 def assert_model(response):
     assert len(response) != 0
@@ -16,14 +13,24 @@ def assert_model(response):
 
 
 class TestChat:
-    @pytest.mark.parametrize("model_name", [
-        "internlm/internlm2-chat-7b",
-        "internlm/internlm2-chat-7b-sft",
-    ])
+    """
+    Test cases for chat model.
+    """
+
+    @pytest.mark.parametrize(
+        "model_name",
+        [
+            "internlm/internlm2-chat-7b",
+            "internlm/internlm2-chat-7b-sft",
+        ],
+    )
     def test_demo_default(self, model_name):
         tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        # Set `torch_dtype=torch.float16` to load model in float16, otherwise it will be loaded as float32 and might cause OOM Error.
-        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, trust_remote_code=True).cuda()
+        # Set `torch_dtype=torch.float16` to load model in float16, otherwise
+        # it will be loaded as float32 and might cause OOM Error.
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype=torch.float16, trust_remote_code=True
+        ).cuda()
         model = model.eval()
         for prompt in prompts:
             response, history = model.chat(tokenizer, prompt, history=[])
@@ -39,19 +46,35 @@ class TestChat:
 
 
 class TestBase:
-    @pytest.mark.parametrize("model_name", [
-        "internlm/internlm2-7b",
-        "internlm/internlm2-base-7b",
-    ])
+    """
+    Test cases for base model.
+    """
+
+    @pytest.mark.parametrize(
+        "model_name",
+        [
+            "internlm/internlm2-7b",
+            "internlm/internlm2-base-7b",
+        ],
+    )
     def test_demo_default(self, model_name):
         tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-        # Set `torch_dtype=torch.float16` to load model in float16, otherwise it will be loaded as float32 and might cause OOM Error.
-        model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, trust_remote_code=True).cuda()
+        # Set `torch_dtype=torch.float16` to load model in float16, otherwise
+        # it will be loaded as float32 and might cause OOM Error.
+        model = AutoModelForCausalLM.from_pretrained(
+            model_name, torch_dtype=torch.float16, trust_remote_code=True
+        ).cuda()
         for prompt in prompts:
             inputs = tokenizer(prompt, return_tensors="pt")
-            for k,v in inputs.items():
+            for k, v in inputs.items():
                 inputs[k] = v.cuda()
-            gen_kwargs = {"max_length": 16280, "top_p": 10, "temperature": 1.0, "do_sample": True, "repetition_penalty": 1.0}
+            gen_kwargs = {
+                "max_length": 16280,
+                "top_p": 10,
+                "temperature": 1.0,
+                "do_sample": True,
+                "repetition_penalty": 1.0,
+            }
             output = model.generate(**inputs, **gen_kwargs)
             output = tokenizer.decode(output[0].tolist(), skip_special_tokens=True)
             print(output)
